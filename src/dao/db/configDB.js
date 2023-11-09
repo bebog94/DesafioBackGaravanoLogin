f9857c4365a851d9bd14b96c0dfe54e969a07663
Rodolfo <bebo.garavano@gmail.com>
2023-11-08 22:26:40 -0300

    first commit

---
+++ src/dao/db/configDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+const URI= 
+
+mongoose
+  .connect(URI)
+  .then(() => console.log("Conectado a la base de datos"))
+  .catch((error) => console.log(error));
+
+
+  export default URI ;
